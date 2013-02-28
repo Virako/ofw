e8322d01b4b145d75565e8799c18af177b3af1f1
Victor Ramirez <virako.9@gmail.com>
2013-02-28 04:52:30 +0100

    Restructuring and deleting unused parameters.

--- src/character.cpp
+++ src/character.cpp
@@ -16,7 +16,6 @@
 * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
 #include <stdexcept>
 
 #include "character.hpp"
@@ -24,61 +23,51 @@
 
 using namespace hw;
 
-character::character()
-{
+
+character::character() {
     this->height = HEIGHT_CTE;
     this->width = WIDTH_CTE;
-    this->model = new string("");
-    this->texture = new string("");
-    this->name = new string("");
 }
 
-character::~character()
-{
+character::~character() {
 }
 
-void character::height_inc()
-{
+void character::height_inc() {
     if (this->height + HEIGHT_INC > HEIGHT_MAX)
         throw domain_error("");
     else
         this->height += HEIGHT_INC;
 }
 
-void character::height_dec()
-{
+void character::height_dec() {
     if (this->height - HEIGHT_INC < HEIGHT_MIN)
         throw domain_error("");
     else
         this->height -= HEIGHT_INC;
 }
 
-void character::set_height(int height)
-{
+void character::set_height(int height) {
     if (height < HEIGHT_MIN || height > HEIGHT_MAX)
         throw domain_error("");
     else
         this->height = height;
 }
 
-void character::width_inc()
-{
+void character::width_inc() {
     if (this->width + WIDTH_INC > WIDTH_MAX)
         throw domain_error("");
     else
         this->width += WIDTH_INC;
 }
 
-void character::width_dec()
-{
+void character::width_dec() {
     if (this->width - WIDTH_INC < WIDTH_MIN)
         throw domain_error("");
     else
         this->width -= WIDTH_INC;
 }
 
-void character::set_width(int width)
-{
+void character::set_width(int width) {
     if (width < WIDTH_MIN || width > WIDTH_MAX)
         throw domain_error("");
     else
