fc36a4dff8dcf5c96ddfaf515ab5c886e56b4c52
Victor Ramirez <virako.9@gmail.com>
2013-03-12 03:31:46 +0100

    Remove using namespace irr.

--- src/main.cpp
+++ src/main.cpp
@@ -19,60 +19,54 @@
 #include <irrlicht.h>
 #include "driverChoice.h"
 
-using namespace irr;
-
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
-int main()
-{
+int main() {
     // ask user for driver
-    video::E_DRIVER_TYPE driverType=driverChoiceConsole();
-    if (driverType==video::EDT_COUNT)
+    irr::video::E_DRIVER_TYPE driverType = irr::driverChoiceConsole();
+    if (driverType == irr::video::EDT_COUNT)
         return 1;
 
     // Create device
-    IrrlichtDevice* device = createDevice(driverType,
-            core::dimension2d<u32>(640, 480), 16, false, false, false, NULL);
+    irr::IrrlichtDevice* device =  irr::createDevice(driverType,
+            irr::core::dimension2d<irr::u32>(640, 480), 16, false, false, false, NULL);
     if (device == 0)
         return 1; // could not create selected driver.
     device->setWindowCaption(L"OFW");
 
     // Load video, scene and gui
-    video::IVideoDriver* driver = device->getVideoDriver();
-    scene::ISceneManager* smgr = device->getSceneManager();
-    gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    irr::scene::ISceneManager* smgr = device->getSceneManager();
+    irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
 
     // SCENE: insert player
-    scene::IAnimatedMesh* player = smgr->getMesh("../media/ninja.b3d");
-    if (!player)
-    {
+    irr::scene::IAnimatedMesh* player = smgr->getMesh("../media/ninja.b3d");
+    if (!player) {
         device->drop();
         return 1;
     }
-    scene::IAnimatedMeshSceneNode* player_ani = smgr->addAnimatedMeshSceneNode(player);
-    if (player_ani)
-    {
-        player_ani->setMaterialFlag(video::EMF_LIGHTING, false);
+    irr::scene::IAnimatedMeshSceneNode* player_ani = smgr->addAnimatedMeshSceneNode(player);
+    if (player_ani) {
+        player_ani->setMaterialFlag(irr::video::EMF_LIGHTING, false);
         player_ani->setFrameLoop(0, 13);  // nº frame
         player_ani->setAnimationSpeed(10); // fps
         player_ani->setMaterialTexture(0, driver->getTexture("../media/ninja.jpg"));
-        player_ani->setScale(core::vector3df(1,1,1));
-        player_ani->setPosition(core::vector3df(0,0,0));
-        player_ani->setRotation(core::vector3df(0,0,0));
+        player_ani->setScale(irr::core::vector3df(1,1,1));
+        player_ani->setPosition(irr::core::vector3df(0,0,0));
+        player_ani->setRotation(irr::core::vector3df(0,0,0));
     }
 
     // SCENE: insert camera
-    scene::ICameraSceneNode* camera = smgr->addCameraSceneNode(player_ani);
-    camera->setPosition(core::vector3df(0, 15, 20));
+    irr::scene::ICameraSceneNode* camera = smgr->addCameraSceneNode(player_ani);
+    camera->setPosition(irr::core::vector3df(0, 15, 20));
     camera->setTarget(player_ani->getPosition());
 
     // BUCLE
-    while(device->run())
-    {
-        driver->beginScene(true, true, video::SColor(255,113,113,133));
+    while(device->run()) {
+        driver->beginScene(true, true, irr::video::SColor(255,113,113,133));
         smgr->drawAll();
         guienv->drawAll(); // draw the gui environment
         driver->endScene();
