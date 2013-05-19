8585773080e9bf0333e91f8f311ba6b181acaa57
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2013-05-19 03:43:26 +0200

    Added irrlicht event handler

---
+++ src/gui/irr_event_receiver.cpp
@@ -0,0 +1,81 @@
+/*************************************************************************
+* Open Fantasy World is a MMORPG where the players will live in community.
+* Copyright (C) 2012-2013 Javier Jaramago Fernández
+* <jaramago.fernandez.javier@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Foobar is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "irrlicht.h"
+#include "irr_event_receiver.hpp"
+
+namespace ofw {
+    namespace gui {
+
+        struct SMouseState
+        {
+            irr::core::position2di Position;
+            bool LeftButtonDown;
+            SMouseState() : LeftButtonDown(false) { }
+        } MouseState;
+
+        bool irrEventReceiver::OnEvent(const irr::SEvent& event)
+        {
+            if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+            {
+                switch(event.MouseInput.Event)
+                {
+                    case irr::EMIE_LMOUSE_PRESSED_DOWN:
+                        MouseState.LeftButtonDown = true;
+                        break;
+
+                    case irr::EMIE_LMOUSE_LEFT_UP:
+                        MouseState.LeftButtonDown = false;
+                        break;
+
+                    case irr::EMIE_MOUSE_MOVED:
+                        MouseState.Position.X = event.MouseInput.X;
+                        MouseState.Position.Y = event.MouseInput.Y;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            if (event.EventType == irr::EET_JOYSTICK_INPUT_EVENT
+                && event.JoystickEvent.Joystick == 0)
+            {
+                JoystickState = event.JoystickEvent;
+            }
+
+            return false;
+        }
+
+        const irr::SEvent::SJoystickEvent& irrEventReceiver::GetJoystickState(void) const
+        {
+            return JoystickState;
+        }
+
+        const SMouseState& irrEventReceiver::GetMouseState(void) const
+        {
+            return MouseState;
+        }
+
+
+        irrEventReceiver::irrEventReceiver()
+        {
+        }
+    }
+}
---
+++ src/gui/irr_event_receiver.hpp
@@ -0,0 +1,68 @@
+/*************************************************************************
+* Open Fantasy World is a MMORPG where the players will live in community.
+* Copyright (C) 2012-2013 Javier Jaramago Fernández
+* <jaramago.fernandez.javier@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Foobar is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef IRRLICHT_EVENT_RECEIVER_HPP
+#define IRRLICHT_EVENT_RECEIVER_HPP
+
+#include "IEventReceiver.h"
+
+namespace ofw {
+    namespace gui {
+        class SMouseState;
+    }
+}
+
+namespace ofw {
+    namespace gui {
+
+        class irrEventReceiver : public irr::IEventReceiver {
+            private:
+                irr::SEvent::SJoystickEvent JoystickState;
+            public:
+                /**
+                 * @brief Default constructor.
+                 */
+                irrEventReceiver();
+                /**
+                 * @brief Handles the events that Irrlicth recieves from the
+                 * windows system.
+                 *
+                 * @param event Event that need to be handle.
+                 *
+                 * @return returns true when the events has been completely
+                 * handled, if not return false.
+                 */
+                virtual bool OnEvent(const irr::SEvent& event);
+                /**
+                 * @brief Get the actual joystick state.
+                 *
+                 * @return Joystick state.
+                 */
+                const irr::SEvent::SJoystickEvent& GetJoystickState(void) const;
+                /**
+                 * @brief Get the actual mouse state.
+                 *
+                 * @return Mouse state.
+                 */
+                const SMouseState& GetMouseState(void) const;
+        };
+    }
+}
+
+#endif
